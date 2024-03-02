553af21c8ee709450b9f9ad414f5af8cbeb76209
joaobug <joaoenfrance@gmail.com>
2024-03-02 11:50:34 -0300

    Initial Commit

---
+++ AnimatedTicTac/AnimatedTicTacApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedTicTacApp.swift
+//  AnimatedTicTac
+//
+//  Created by Joao Leal on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedTicTacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
